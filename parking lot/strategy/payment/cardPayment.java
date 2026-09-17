2eba61f0723f13d8bb7abc3e7d7a74e0489bb4c8
Anisha Ray <rayanisha620@gmail.com>
2026-09-17 23:37:53 +0530

    Create cardPayment.java

---
+++ parking lot/strategy/payment/cardPayment.java
@@ -0,0 +1,11 @@
+package org.nailyourinterview.lld.parking_lot.strategy.payment;
+
+import org.nailyourinterview.lld.parking_lot.model.Ticket;
+
+public class CardPayment implements PaymentStrategy {
+    @Override
+    public boolean processPayment(Ticket ticket, double amount) {
+        System.out.println("Paid ₹" + amount + " for ticket " + ticket.getTicketId() + " via Card.");
+        return true;
+    }
+}
