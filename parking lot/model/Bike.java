87c72663a5216c041f0c0004c42e6a943c2ee79c
Anisha Ray <rayanisha620@gmail.com>
2026-09-18 00:00:22 +0530

    Add Bike class extending Vehicle

---
+++ parking lot/model/Bike.java
@@ -0,0 +1,7 @@
+package org.nailyourinterview.lld.parking_lot.model;
+
+import org.nailyourinterview.lld.parking_lot.enums.VehicleType;
+
+public class Bike extends Vehicle {
+    public Bike(String number) { super(number, VehicleType.BIKE); }
+}
