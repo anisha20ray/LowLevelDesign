9089292dd5d86f9f389e0f0ca9cb11dc8f0aed79
Anisha Ray <rayanisha620@gmail.com>
2026-09-17 23:38:51 +0530

    Create cashPayment.java

---
+++ parking lot/strategy/payment/cashPayment.java
@@ -0,0 +1,11 @@
+package org.nailyourinterview.lld.parking_lot.strategy.payment;
+
+import org.nailyourinterview.lld.parking_lot.model.Ticket;
+
+public class CashPayment implements PaymentStrategy {
+    @Override
+    public boolean processPayment(Ticket ticket, double amount) {
+        System.out.println("Paid ₹" + amount + " for ticket " + ticket.getTicketId() + " via Cash.");
+        return true;
+    }
+}
